593aba2e6d92a285b1e8b26e84262fa1dd30d330
Christian Jensen <cbejensen@gmail.com>
2022-12-01 10:40:03 -0700

    controlling currentTime... almost works

--- src/lib/track/Tracks.svelte
+++ src/lib/track/Tracks.svelte
@@ -5,23 +5,32 @@
 
   export let tracks: TrackType[];
 
-  let audioCtx: AudioContext;
-  let audioEls: NodeListOf<HTMLAudioElement>;
+  let audioCtx: AudioContext | undefined;
+  let audioEls: NodeListOf<HTMLAudioElement> | undefined;
   let paused = true;
-  console.log(paused);
+
   let currentTime = 0;
+  $: duration = audioEls?.[0].duration;
+
+  function formatTime(timeInSeconds: number) {
+    const minutes = Math.floor(timeInSeconds / 60);
+    let seconds: string | number = Math.floor(timeInSeconds % 60);
+    if (seconds < 10) seconds = '0' + seconds;
+
+    return `${minutes}:${seconds}`;
+  }
 
   onMount(() => {
     audioCtx = new AudioContext();
     audioEls = document.querySelectorAll('.track');
     audioEls.forEach((el) => {
-      const track = audioCtx.createMediaElementSource(el);
-      track.connect(audioCtx.destination);
+      const track = audioCtx!.createMediaElementSource(el);
+      track.connect(audioCtx!.destination);
     });
   });
 
   function togglePlay() {
-    if (audioCtx.state === 'suspended') {
+    if (audioCtx?.state === 'suspended') {
       audioCtx.resume();
     }
     paused = !paused;
@@ -31,23 +40,46 @@
 </script>
 
 <div class="tracks">
-  {#each tracks as track}
-    <audio
-      class="track"
-      src={track.url}
-      bind:paused
-      muted={track.muted || (someSoloed && !track.soloed)}
-    />
+  {#each tracks as track, i}
+    {#if i === 0}
+      <!-- content here -->
+      <audio
+        class="track"
+        src={track.url}
+        bind:paused
+        bind:currentTime
+        muted={track.muted || (someSoloed && !track.soloed)}
+      />
+    {:else}
+      <audio
+        class="track"
+        src={track.url}
+        bind:paused
+        {currentTime}
+        muted={track.muted || (someSoloed && !track.soloed)}
+      />
+    {/if}
     <Track
       {track}
-      implicitlyMuted={someSoloed}
+      implicitlyMuted={someSoloed && !track.soloed}
       on:toggleMute={() => (track.muted = !track.muted)}
       on:toggleSolo={() => (track.soloed = !track.soloed)}
     />
   {/each}
 </div>
 <button on:click={togglePlay} type="button">{paused ? 'Play' : 'Pause'}</button>
-<p>Current time: {currentTime}</p>
+<p>Current time: {formatTime(currentTime)}</p>
+{#if duration}
+  <p>Duration: {formatTime(duration)}</p>
+  <input
+    type="range"
+    name="seek song"
+    bind:value={currentTime}
+    min={0}
+    max={duration}
+    step={0.01}
+  />
+{/if}
 
 <style>
   .tracks {
