b85033bbdc693719b323fb6a6e269966749ecd90
Christian Jensen <cbejensen@gmail.com>
2022-12-29 00:25:37 -0700

    switch to postgres

--- prisma/migrations/20221215025315_notation/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Arrangement" ADD COLUMN "tempo" INTEGER;
-ALTER TABLE "Arrangement" ADD COLUMN "timeSignature" TEXT;
-
--- AlterTable
-ALTER TABLE "Track" ADD COLUMN "notation" TEXT;
