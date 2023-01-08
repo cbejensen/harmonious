5b7a8f918f0d17989726274cfd54a9b02d0acf88
Christian Jensen <cbejensen@gmail.com>
2023-01-07 22:35:42 -0700

    try to get data from server

--- src/routes/songs/+page.server.ts
+++ src/routes/songs/+page.server.ts
@@ -2,6 +2,7 @@ import { db } from '$lib/server/db';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
+  return { songs: [{ name: 'test', id: '333' }] };
   // const songs = await db.song.findMany();
   // return { songs };
 };
