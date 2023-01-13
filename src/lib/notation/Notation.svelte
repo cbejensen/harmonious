8dd552e5adee3c81edf1db9e2c35a54e07d4080d
Christian Jensen <cbejensen@gmail.com>
2023-01-12 23:22:26 -0700

    sync notation with song when played before mount

--- src/lib/notation/Notation.svelte
+++ src/lib/notation/Notation.svelte
@@ -1,21 +1,20 @@
 <script lang="ts">
-  import abcjs, { TimingCallbacks } from 'abcjs';
-  import { onMount } from 'svelte';
+  import { renderAbc, TimingCallbacks, type TuneObject } from 'abcjs';
+  import { onMount, onDestroy } from 'svelte';
 
   import { songStore } from '../songStore';
 
-  let mounted = false;
-  let viewMode: 'horizontal' | 'vertical' = 'vertical';
+  let abcjsInstance: TuneObject | undefined;
   let timingCallbacks: TimingCallbacks | undefined;
-  $: if (mounted) {
-    const abcjsInstance = abcjs.renderAbc('notation', notation, {
+
+  onMount(() => {
+    abcjsInstance = renderAbc('notation', notation, {
       selectTypes: false,
       staffwidth: 800,
       responsive: 'resize',
       wrap: { minSpacing: 1.8, maxSpacing: 2.7, preferredMeasuresPerLine: 4 }
     })[0];
-
-    timingCallbacks = new abcjs.TimingCallbacks(abcjsInstance, {
+    timingCallbacks = new TimingCallbacks(abcjsInstance, {
       eventCallback: (ev) => {
         if (ev) {
           // Not sure if the typing is just wrong or what, but this appears to be an array of arrays.
@@ -25,64 +24,60 @@
         }
       }
     });
-
-    function highlightElements(voiceEls: HTMLElement[][]) {
-      document
-        .querySelectorAll('.abcjs-highlighted')
-        .forEach((el) => el.classList.remove('abcjs-highlighted'));
-      voiceEls.forEach((chordEls) => {
-        chordEls.forEach((noteEl) => {
-          console.log(noteEl);
-          noteEl.classList.add('abcjs-highlighted');
-        });
-      });
+    if (!$songStore.paused) {
+      timingCallbacks.start($songStore.currentTime, 'seconds');
     }
-  }
+  });
 
-  onMount(() => {
-    mounted = true;
+  onDestroy(() => {
+    timingCallbacks?.stop();
   });
 
   let prevPaused: boolean | undefined;
   $: if ($songStore.paused && !prevPaused) {
     prevPaused = true;
-    timingCallbacks?.pause();
+    timingCallbacks?.stop();
   } else if (!$songStore.paused && prevPaused !== false) {
     prevPaused = false;
     timingCallbacks?.start($songStore.currentTime, 'seconds');
   }
 
-  let notation: string;
-  $: {
-    const {
-      arrangement: { key, tempo, timeSignature },
-      tracks
-    } = $songStore;
-
-    const trackNotation = tracks
-      .map((track, i) => {
-        if (!track.notation) {
-          return;
-        }
-        const trackNotation = track.notation
-          .split('\\n')
-          .join(viewMode === 'horizontal' ? '' : '\n');
-        return [
-          `V:${i} name="${track.name}" sname="${track.shortName ?? track.name.slice(0, 3)}" clef="${
-            track.clef
-          }"`,
-          trackNotation
-        ].join('\n');
-      })
-      .join('\n')
-      .trim();
-
-    notation = [`X:1`, `M:${timeSignature}`, `Q:${tempo}`, `K:${key}`, trackNotation].join('\n');
+  function highlightElements(voiceEls: HTMLElement[][]) {
+    document
+      .querySelectorAll('.abcjs-highlighted')
+      .forEach((el) => el.classList.remove('abcjs-highlighted'));
+    voiceEls.forEach((chordEls) => {
+      chordEls.forEach((noteEl) => {
+        noteEl.classList.add('abcjs-highlighted');
+      });
+    });
   }
+
+  const {
+    arrangement: { key, tempo, timeSignature },
+    tracks
+  } = $songStore;
+
+  $: trackNotation = tracks
+    .map((track, i) => {
+      if (!track.notation) {
+        return;
+      }
+      return [
+        `V:${i} name="${track.name}" sname="${track.shortName ?? track.name.slice(0, 3)}" clef="${
+          track.clef
+        }"`,
+        track.notation
+      ].join('\n');
+    })
+    .join('\n')
+    .trim();
+
+  $: notation = [`X:1`, `M:${timeSignature}`, `Q:${tempo}`, `K:${key}`, trackNotation].join('\n');
 </script>
 
 <div class="wrap">
-  <div class={`notation ${viewMode}`} id="notation" />
+  <div class="notation" id="notation" />
 </div>
 
 <style>
@@ -91,19 +86,19 @@
     max-width: 1000px;
   }
 
-  .notation.horizontal {
-    /* Override obnoxious styling from abcjs, which applies them as inline styles */
+  /* Override obnoxious styling from abcjs, which applies them as inline styles */
+  /* .notation.horizontal {
     display: block !important;
     overflow-x: auto !important;
-  }
+  } */
 
   :global(.notation > svg) {
     margin: auto;
   }
 
-  :global(.notation.horizontal > svg) {
+  /* :global(.notation.horizontal > svg) {
     max-width: none !important;
-  }
+  } */
 
   :global(.abcjs-highlighted) {
     fill: goldenrod;
